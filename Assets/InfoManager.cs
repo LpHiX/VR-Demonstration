74b64c3f115a0a583285319023cddb83f760e2da
LpHiX <martin.leung.ht@gmail.com>
2022-07-12 16:33:52 +0800

    upgrade to URP

--- Assets/InfoManager.cs
+++ Assets/InfoManager.cs
@@ -8,9 +8,12 @@ public class InfoManager : MonoBehaviour
     public static InfoManager _instance;
     public TextMeshProUGUI TextComponent;
     public GameObject TextPanel;
+    public GameObject SelectCanvas;
+    public Transform cameraTransform;
 
-    private Transform planetTransform;
-    private Transform cameraTransform;
+    public Transform planetTransform;
+    private Transform selectTransform;
+    private RectTransform infoTransform;
     private void Awake()
     {
         if(_instance != null && _instance != this)
@@ -21,11 +24,13 @@ public class InfoManager : MonoBehaviour
         {
             _instance = this;
         }
+        this.infoTransform = TextPanel.GetComponent<RectTransform>();
     }
     // Start is called before the first frame update
     void Start()
     {
         TextPanel.SetActive(false);
+        SelectCanvas.SetActive(false);
     }
 
     // Update is called once per frame
@@ -35,20 +40,37 @@ public class InfoManager : MonoBehaviour
         {
             TextPanel.transform.position = planetTransform.position;
             TextPanel.transform.rotation = Quaternion.LookRotation(planetTransform.position - cameraTransform.position, Vector3.up);
-
+            TextPanel.transform.localScale = 0.003f * Vector3.one * Vector3.Distance(planetTransform.position, cameraTransform.position) * Mathf.Tan(Mathf.Deg2Rad * 60f * 0.5f);
+            infoTransform.pivot = new Vector2(-10 * Mathf.Sqrt(selectTransform.localScale.x) / Vector3.Distance(planetTransform.position, cameraTransform.position), 0.5f);
+        }
+        if (SelectCanvas.activeSelf)
+        {
+            SelectCanvas.transform.position = selectTransform.position;
+            SelectCanvas.transform.rotation = Quaternion.LookRotation(selectTransform.position - cameraTransform.position, Vector3.up);
+            SelectCanvas.transform.localScale = 0.001f * Mathf.Sqrt(selectTransform.localScale.x)  * Vector3.one * Vector3.Distance(selectTransform.position, cameraTransform.position) * Mathf.Tan(Mathf.Deg2Rad * 60f * 0.5f);
         }
     }
-    public void SetInfoBox(string Message, Transform planetTransform, Transform cameraTransform)
+    public void SetInfoBox(string Message, Transform planetTransform)
     {
         this.planetTransform = planetTransform;
-        this.cameraTransform = cameraTransform;
 
         TextPanel.SetActive(true);
         TextComponent.text = Message;
     }
     public void HideInfoBox()
     {
+        this.planetTransform = null;
+
         TextPanel.SetActive(false);
         TextComponent.text = string.Empty;
+    }
+    public void ShowSelector(Transform selectTransform)
+    {
+        this.selectTransform = selectTransform;
+        SelectCanvas.SetActive(true);
+    }
+    public void HideSelector()
+    {
+        SelectCanvas.SetActive(false);
     }
 }
--- Assets/InfoScript.cs
+++ Assets/InfoScript.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class InfoScript : MonoBehaviour
 {
     public string Message;
     [SerializeField]
-    private Transform CameraTransform;
-    public void HoverMethod()
+    public void Awake()
     {
-        InfoManager._instance.SetInfoBox(Message, transform, CameraTransform);
+        XRBaseInteractable interactable = GetComponent<XRBaseInteractable>();
+        interactable.hoverEntered.AddListener(HoverMethod);
+        interactable.hoverExited.AddListener(HoverExitMethod);
+        interactable.selectEntered.AddListener(SelectMethod);
     }
-    public void HoverExitMethod()
+
+    private void SelectMethod(SelectEnterEventArgs arg0)
     {
-        InfoManager._instance.HideInfoBox();
+        if (InfoManager._instance.planetTransform == transform)
+        {
+            InfoManager._instance.HideInfoBox();
+            Debug.Log("Hiding");
+        }
+        else
+        {
+            InfoManager._instance.SetInfoBox(Message, transform);
+            Debug.Log("Showing");
+        }
+    }
+
+    private void HoverMethod(HoverEnterEventArgs arg0)
+    {
+        InfoManager._instance.ShowSelector(transform);
+    }
+
+    private void HoverExitMethod(HoverExitEventArgs arg0)
+    {
+        InfoManager._instance.HideSelector();
     }
 }
